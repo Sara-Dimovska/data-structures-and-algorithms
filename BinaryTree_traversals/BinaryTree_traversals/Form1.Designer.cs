14e70bef80a833b23e9745f75d81aba9340e8ae4
Sara Kostovska <Sara Kostovska>
2017-06-30 18:37:51 +0200

    done

---
+++ BinaryTree_traversals/BinaryTree_traversals/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace BinaryTree_traversals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.inorderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preorderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(172, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(368, 29);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(172, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(368, 29);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(172, 272);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(368, 29);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(583, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "InOrder:(LOD)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "PreOrder:(OLD)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PostOrder:(LDO)";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(752, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inputToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // inputToolStripMenuItem
+            // 
+            this.inputToolStripMenuItem.Name = "inputToolStripMenuItem";
+            this.inputToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.N)));
+            this.inputToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.inputToolStripMenuItem.Text = "Input";
+            this.inputToolStripMenuItem.Click += new System.EventHandler(this.inputToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inputToolStripMenuItem1});
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // inputToolStripMenuItem1
+            // 
+            this.inputToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inorderToolStripMenuItem,
+            this.preorderToolStripMenuItem});
+            this.inputToolStripMenuItem1.Name = "inputToolStripMenuItem1";
+            this.inputToolStripMenuItem1.Size = new System.Drawing.Size(102, 22);
+            this.inputToolStripMenuItem1.Text = "Input";
+            // 
+            // inorderToolStripMenuItem
+            // 
+            this.inorderToolStripMenuItem.Name = "inorderToolStripMenuItem";
+            this.inorderToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
+            this.inorderToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.inorderToolStripMenuItem.Text = "Inorder";
+            this.inorderToolStripMenuItem.Click += new System.EventHandler(this.inorderToolStripMenuItem_Click);
+            // 
+            // preorderToolStripMenuItem
+            // 
+            this.preorderToolStripMenuItem.Name = "preorderToolStripMenuItem";
+            this.preorderToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.preorderToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.preorderToolStripMenuItem.Text = "Preorder";
+            this.preorderToolStripMenuItem.Click += new System.EventHandler(this.preorderToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F1)));
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 386);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Binary Tree traversals";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inputToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inputToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem inorderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem preorderToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ BinaryTree_traversals/BinaryTree_traversals/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BinaryTree_traversals
+{
+
+    public partial class Form1 : Form
+    {
+        //klasa za jazel
+        public class Node
+        {
+            public int value;
+            public Node left, right;
+
+            public Node(){}
+            public Node(int value)
+            {
+                this.value = value;
+                left = right = null;
+            }
+
+        }
+
+        public class BinaryTree
+        {
+            //so ovaa promenliva gi izminuvame site clenovi na preorder od pocetok do kraj 
+            // sekoj nareden clen/jazol od preorder,se bara vo inorder
+            int preIndex = 0;
+
+            public Node Tree(int[] inorder, int[] preorder, int pocetok, int kraj)
+            {
+
+
+                // nevozmozno da se izgradi drvo 
+                if (pocetok > kraj)
+                    return null;
+
+                //se formira  jazol 
+                Node node = new Node(preorder[preIndex++]);
+
+                //ako ima samo edinstven clen/eden jazol
+                if (pocetok == kraj)
+                    return node;
+
+                //se bara indeksot vo inorder nizata za da ponatamu se formiraat podnizi
+                int in_Index = search(inorder, pocetok, kraj, node.value);
+                
+
+
+                //za levo i desno podsteblo na jazol,rekurzivno
+                node.left = Tree(inorder, preorder, pocetok, in_Index - 1);
+                node.right = Tree(inorder, preorder, in_Index + 1, kraj);
+
+                return node;
+            }
+            
+
+
+            //pomoshna,se upotrebuva za da se najde indeksot na barana vrednost
+            //vo slucajot ja koristime za vrednosta zemena od preorder
+            public int search(int[] arr, int start, int end, int value)
+            {
+                int i;
+                for (i = start; i <= end; i++)
+                {
+                    if (arr[i] == value)
+                        break;
+
+                }
+                return i;
+            }
+
+            //output
+            public string postorder(Node root)
+            {
+               
+                string izlez = "";
+
+                if (root == null)
+                    return "";
+
+                Stack<Node> stack1 = new Stack<Node>();//pomoshen
+                Stack<Node> stack2 = new Stack<Node>();//podreden 
+
+                stack1.Push(root);
+
+                Node temp = new Node();
+                while (stack1.Count() > 0)
+                {
+                    temp = stack1.Pop();
+                    //vo vtoriot stek kje se stavaat koren,desno,levo za da moze koga kje se vadat
+                    //da bide levo,desno,koren
+                    stack2.Push(temp);
+
+                    if (temp.left != null)
+                        stack1.Push(temp.left);
+
+                    if (temp.right != null)
+                        stack1.Push(temp.right);
+                }
+
+                //smestuvame vo string
+                while (stack2.Count() > 0)
+                {
+                    temp = stack2.Pop();
+                    izlez += temp.value.ToString() + ",";
+                }
+                return izlez;
+            }
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+            string inorder_string = textBox1.Text;
+            string preorder_string = textBox2.Text;
+
+            
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                button1.Enabled = false;
+
+                //od string vo niza
+                int[] inorder_array = Array.ConvertAll(inorder_string.Split(','), int.Parse);
+                int[] preorder_array = Array.ConvertAll(preorder_string.Split(','), int.Parse);
+
+
+            //mora da imaat isti dolzini dvete nizi
+            if (Validiraj_dolzini(inorder_array, preorder_array))
+            {
+                //instanca 
+                BinaryTree tree = new BinaryTree();
+
+                //dolzina
+                int n = inorder_array.Length;
+
+                //call
+                Node root = tree.Tree(inorder_array, preorder_array, 0, n - 1);
+
+                //output
+                string izlez = tree.postorder(root);
+                textBox3.Text = izlez;
+            }
+           
+
+        }
+        private  bool Validiraj_dolzini(int [] niza1,int [] niza2)
+        {
+            if (niza1.Length == niza2.Length)
+                return true;
+            else
+            {
+                MessageBox.Show("Vnesete ednakvi dolzini na dvete nizi!");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                button1.Enabled = true;
+                textBox1.Focus();
+
+                return false;
+            }
+        }
+
+        //od pocetok
+        private void inputToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            button1.Enabled = true;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Inorder vnes:Levo,Roditel,Desno dete. \n Preorder vnes:Roditel,Levo,Desno. \n Ve molime zapazete go redosledot.");
+
+        }
+
+        //validacija na vnesovi
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != ',' && (!char.IsDigit(e.KeyChar)) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != ',' &&  (!char.IsDigit(e.KeyChar)) && !char.IsControl(e.KeyChar) )
+                e.Handled = true;
+        }
+
+       
+        //datoteki
+        private void inorderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = " All files (*.*)| *.*";
+            openFileDialog1.Title = "proba";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.Text = openFileDialog1.FileName;
+                System.IO.StreamReader text = new StreamReader(openFileDialog1.FileName);
+                textBox1.Text = text.ReadToEnd();
+                text.Close();
+            }
+        }
+
+        private void preorderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = " All files (*.*)| *.*";
+            openFileDialog1.Title = "proba";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.Text = openFileDialog1.FileName;
+                System.IO.StreamReader text = new StreamReader(openFileDialog1.FileName);
+                textBox2.Text = text.ReadToEnd();
+                text.Close();
+            }
+        }
+    }
+}
+//primer:
+// in: 3,5,7,10,13,15,17,20,21,25,28,30,26,40,45";
+// pre:"20,10,5,3,7,15,13,17,30,25,21,28,40,36,45";
+
+// post:3,7,5,13,17,15,10,21,28,25,36,45,40,30,20,
